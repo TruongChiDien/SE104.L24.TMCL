74eeef355ae7a0146e4a10c7e3151c82696e9caa
Dien Truong Chi <dientc1501@gmail.com>
2021-06-19 09:16:51 +0700

    "Them chuc nang KhachHang, sua form Dashboard_NhanVien"

--- QuanLyNhaSach/QuanLyNhaSach/Forms/Dashboard_NhanVien.Designer.cs
+++ QuanLyNhaSach/QuanLyNhaSach/Forms/Dashboard_NhanVien.Designer.cs
@@ -68,9 +68,10 @@ namespace QuanLyNhaSach.Forms
             this.button3.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(112)))), ((int)(((byte)(85)))));
             this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
-            this.button3.Location = new System.Drawing.Point(-1004, 0);
+            this.button3.Location = new System.Drawing.Point(-483, 0);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(94, 81);
+            this.button3.Size = new System.Drawing.Size(58, 51);
             this.button3.TabIndex = 5;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -81,20 +82,24 @@ namespace QuanLyNhaSach.Forms
             this.panel4.Controls.Add(this.button4);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel4.Location = new System.Drawing.Point(350, 0);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(1260, 80);
             this.panel4.TabIndex = 4;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Century Gothic", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(112)))), ((int)(((byte)(85)))));
-            this.label8.Location = new System.Drawing.Point(138, 9);
+            this.label8.Location = new System.Drawing.Point(63, 15);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(245, 49);
+            this.label8.Size = new System.Drawing.Size(194, 40);
             this.label8.TabIndex = 8;
             this.label8.Text = "Book Store";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
             // button4
             // 
@@ -102,9 +107,10 @@ namespace QuanLyNhaSach.Forms
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.ForeColor = System.Drawing.Color.White;
             this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
-            this.button4.Location = new System.Drawing.Point(1180, 0);
+            this.button4.Location = new System.Drawing.Point(1211, 0);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(80, 80);
+            this.button4.Size = new System.Drawing.Size(49, 80);
             this.button4.TabIndex = 0;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click_1);
@@ -118,9 +124,11 @@ namespace QuanLyNhaSach.Forms
             this.panelLeft.Controls.Add(this.panel3);
             this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft.Margin = new System.Windows.Forms.Padding(2);
             this.panelLeft.Name = "panelLeft";
             this.panelLeft.Size = new System.Drawing.Size(350, 927);
             this.panelLeft.TabIndex = 3;
+            this.panelLeft.Paint += new System.Windows.Forms.PaintEventHandler(this.panelLeft_Paint);
             // 
             // btnKhachHang
             // 
@@ -130,7 +138,8 @@ namespace QuanLyNhaSach.Forms
             this.btnKhachHang.ForeColor = System.Drawing.Color.White;
             this.btnKhachHang.Image = ((System.Drawing.Image)(resources.GetObject("btnKhachHang.Image")));
             this.btnKhachHang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnKhachHang.Location = new System.Drawing.Point(10, 392);
+            this.btnKhachHang.Location = new System.Drawing.Point(10, 380);
+            this.btnKhachHang.Margin = new System.Windows.Forms.Padding(2);
             this.btnKhachHang.Name = "btnKhachHang";
             this.btnKhachHang.Size = new System.Drawing.Size(340, 140);
             this.btnKhachHang.TabIndex = 3;
@@ -141,10 +150,12 @@ namespace QuanLyNhaSach.Forms
             // panelTicked
             // 
             this.panelTicked.BackColor = System.Drawing.Color.White;
-            this.panelTicked.Location = new System.Drawing.Point(0, 237);
+            this.panelTicked.Location = new System.Drawing.Point(0, 240);
+            this.panelTicked.Margin = new System.Windows.Forms.Padding(2);
             this.panelTicked.Name = "panelTicked";
             this.panelTicked.Size = new System.Drawing.Size(8, 140);
             this.panelTicked.TabIndex = 2;
+            this.panelTicked.Paint += new System.Windows.Forms.PaintEventHandler(this.panelTicked_Paint);
             // 
             // btnBanSach
             // 
@@ -155,7 +166,8 @@ namespace QuanLyNhaSach.Forms
             this.btnBanSach.ForeColor = System.Drawing.Color.White;
             this.btnBanSach.Image = ((System.Drawing.Image)(resources.GetObject("btnBanSach.Image")));
             this.btnBanSach.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnBanSach.Location = new System.Drawing.Point(10, 246);
+            this.btnBanSach.Location = new System.Drawing.Point(10, 240);
+            this.btnBanSach.Margin = new System.Windows.Forms.Padding(2);
             this.btnBanSach.Name = "btnBanSach";
             this.btnBanSach.Size = new System.Drawing.Size(340, 140);
             this.btnBanSach.TabIndex = 0;
@@ -172,9 +184,11 @@ namespace QuanLyNhaSach.Forms
             this.panel3.Controls.Add(this.label1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(350, 240);
             this.panel3.TabIndex = 2;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint_1);
             // 
             // button1
             // 
@@ -183,9 +197,10 @@ namespace QuanLyNhaSach.Forms
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Image = global::QuanLyNhaSach.Properties.Resources.menu_64px1;
-            this.button1.Location = new System.Drawing.Point(261, -2);
+            this.button1.Location = new System.Drawing.Point(282, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 82);
+            this.button1.Size = new System.Drawing.Size(68, 63);
             this.button1.TabIndex = 7;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
@@ -194,23 +209,27 @@ namespace QuanLyNhaSach.Forms
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
-            this.pictureBox2.Location = new System.Drawing.Point(108, 9);
+            this.pictureBox2.Location = new System.Drawing.Point(109, 32);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(132, 120);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(92, 144);
+            this.label1.Location = new System.Drawing.Point(119, 172);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(166, 36);
+            this.label1.Size = new System.Drawing.Size(107, 23);
             this.label1.TabIndex = 3;
             this.label1.Text = "Book Store";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // timer1
             // 
@@ -231,9 +250,11 @@ namespace QuanLyNhaSach.Forms
             this.panel5.Controls.Add(this.label2);
             this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel5.Location = new System.Drawing.Point(350, 80);
+            this.panel5.Margin = new System.Windows.Forms.Padding(2);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(1260, 163);
             this.panel5.TabIndex = 5;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
             // 
             // labelRealTime
             // 
@@ -241,9 +262,10 @@ namespace QuanLyNhaSach.Forms
             this.labelRealTime.AutoSize = true;
             this.labelRealTime.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.labelRealTime.ForeColor = System.Drawing.Color.White;
-            this.labelRealTime.Location = new System.Drawing.Point(973, 62);
+            this.labelRealTime.Location = new System.Drawing.Point(1019, 62);
+            this.labelRealTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelRealTime.Name = "labelRealTime";
-            this.labelRealTime.Size = new System.Drawing.Size(144, 38);
+            this.labelRealTime.Size = new System.Drawing.Size(90, 23);
             this.labelRealTime.TabIndex = 8;
             this.labelRealTime.Text = "realtime";
             this.labelRealTime.Click += new System.EventHandler(this.labelRealTime_Click);
@@ -253,65 +275,75 @@ namespace QuanLyNhaSach.Forms
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(348, 92);
+            this.label6.Location = new System.Drawing.Point(327, 92);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(174, 38);
+            this.label6.Size = new System.Drawing.Size(109, 23);
             this.label6.TabIndex = 7;
             this.label6.Text = "Nhân viên";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(211, 92);
+            this.label5.Location = new System.Drawing.Point(190, 92);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(106, 38);
+            this.label5.Size = new System.Drawing.Size(67, 23);
             this.label5.TabIndex = 6;
             this.label5.Text = "Rules:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(337, 34);
+            this.label4.Location = new System.Drawing.Point(316, 34);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(145, 38);
+            this.label4.Size = new System.Drawing.Size(91, 23);
             this.label4.TabIndex = 5;
             this.label4.Text = "<name>";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(619, 62);
+            this.label3.Location = new System.Drawing.Point(381, 39);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 32);
+            this.label3.Size = new System.Drawing.Size(0, 20);
             this.label3.TabIndex = 5;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(138, 34);
+            this.label2.Location = new System.Drawing.Point(117, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(173, 38);
+            this.label2.Size = new System.Drawing.Size(110, 23);
             this.label2.TabIndex = 4;
             this.label2.Text = "Welcome:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // panelControl
             // 
             this.panelControl.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelControl.Location = new System.Drawing.Point(350, 243);
+            this.panelControl.Margin = new System.Windows.Forms.Padding(2);
             this.panelControl.Name = "panelControl";
             this.panelControl.Size = new System.Drawing.Size(1260, 684);
             this.panelControl.TabIndex = 6;
-            this.panelControl.Paint += new System.Windows.Forms.PaintEventHandler(this.panelControl_Paint);
             // 
             // Dashboard_NhanVien
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1610, 927);
@@ -320,6 +352,7 @@ namespace QuanLyNhaSach.Forms
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panelLeft);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Dashboard_NhanVien";
             this.Text = "Dashboard_NhanVien";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
